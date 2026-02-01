d9da3c727415f87792e3d58a0ab21e4f490468ef
Rózsa Zoltán <67325669+rozsazoltan@users.noreply.github.com>
2026-02-01 12:56:13 +0100

    fix: handle color values correctly in matchUtilities

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import plugin from 'tailwindcss/plugin';
+import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
 import patterns from './patterns.js';
 
 export default plugin(
   ({ matchUtilities, theme }) => {
-    const patterns = theme('pattern', {});
+    const patternList = theme('pattern', {});
+    const colors = flattenColorPalette(theme('colors'));
 
-    for (const [patternName, patternSvg] of Object.entries(patterns)) {
+    for (const [patternName, patternSvg] of Object.entries(patternList)) {
       matchUtilities(
         {
           [`pattern-${patternName}`]: (value, { modifier }) => {
             const opacityValue = modifier !== null ? modifier : '1';
-            const encodedColor = value.toString().replace('#', '%23');
+            
+            const colorStr = Array.isArray(value) ? value[0] : value;
+            const encodedColor = colorStr.toString().replace('#', '%23');
 
             let coloredPattern = patternSvg.replace('{{color}}', encodedColor);
             coloredPattern = coloredPattern.replace('{{opacity}}', opacityValue);
@@ -21,9 +25,9 @@ export default plugin(
           },
         },
         {
-          values: theme('colors'),
-          modifiers: theme('opacity'), 
-          type: ['color', 'any'], 
+          values: colors,
+          modifiers: theme('opacity'),
+          type: ['color', 'any'],
         }
       );
     }
